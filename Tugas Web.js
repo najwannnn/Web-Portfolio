55f2b0ec8c6c527167623378fce766ecf10532fc
najwannnn <alhamdinajwan@gmail.com>
2025-05-29 23:09:24 +0700

    Upload

---
+++ Tugas Web.js
@@ -0,0 +1,12 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      window.scrollTo({
+        top: target.offsetTop - 50, // offset jika header fixed
+        behavior: 'smooth'
+      });
+    }
+  });
+});
